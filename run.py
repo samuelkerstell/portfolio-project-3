56f0d8b678c2731857a43168fcdd9b47e75d2e3e
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-06-19 21:53:31 +0000

    Add function to validate user input

--- run.py
+++ run.py
@@ -1,28 +1,47 @@
 import random
 
 
-print("--------------------")
-print("")
-options = ["left", "right", "middle"]
-computerChoice = random.choice(options)
 
-userChoice = input("Take the penalty choose: left, right or middle: ")
-
-
-
-
-
-def goalCheck():
+def goalCheck(computerChoice, userChoice, score):
     if computerChoice == userChoice:
         print("The goalkeeper has saved your attempt!")
     else:
         print("You scored!")
+        
 
-def validate_user_choice():
-    try
+
+
+def validate_user_choice(value):
+    try:
+        if value not in ["left", "right", "middle"]:
+            raise ValueError(
+                f"Wrong value submitted you provided {value}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
 
 def main():
-    goalCheck()
+    score = 0
+    options = ["left", "right", "middle"]
+    computerChoice = random.choice(options)
+    for i in range(0,5):
+        while True:
+            print("Please choose where you would like to shoot your penalty")
+            print("Data should only contain: left, right, or middle")
+            print("Example: middle")
+            
+            userChoice = input("Enter your answer here: \n")
+
+            if validate_user_choice(userChoice):
+                print("Data is valid!")
+                break
+        
+        goalCheck(computerChoice, userChoice)
 
 main()
 
