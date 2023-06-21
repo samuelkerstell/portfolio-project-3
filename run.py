e2ff13d16522b45ce3e2719e771c4092e8cddcc4
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-06-21 17:15:50 +0000

    Add a user score and a goalkeeper score

--- run.py
+++ run.py
@@ -2,17 +2,22 @@ import random
 
 
 
-def goalCheck(computerChoice, userChoice):
+def goalCheck(computerChoice, userChoice, score, gkScore):
     """
     Checks the user input against the randomized computer choice
     If they are equal the penalty kick was saved
     If they are not equal penalty kick was scored
     """
     if computerChoice == userChoice:
+        gkScore += 1
         print("The goalkeeper has saved your attempt!\n")
     else:
+        score += 1
         print("You scored!\n")
-        
+    print("Goalkeeper score is " + str(gkScore))
+    print("Your score is " + str(score))
+    return score, gkScore
+
 
 
 def validate_user_choice(value):
@@ -36,23 +41,30 @@ def main():
     """
     Run all game functions
     """
+    gkScore = 0
     score = 0
     options = ["left", "right", "middle"]
-    computerChoice = random.choice(options)
-    for i in range(0,5):
+    print("Welcome to the penalty shootout first to 5\n")
+
+    while score < 5 and gkScore < 5:
         while True:
             print("Please choose where you would like to shoot your penalty")
             print("Data should only contain: left, right, or middle")
-            print("Example: middle")
-            
-            userChoice = input("Enter your answer here: \n")
+            print("Example: middle\n")
+                
+            userChoice = input("Enter your answer here: ")
 
             if validate_user_choice(userChoice):
                 break
-        
-        goalCheck(computerChoice, userChoice)
+
+        computerChoice = random.choice(options)
+        score, gkScore = goalCheck(computerChoice, userChoice, score, gkScore)
+        print()
+    if score == 5:
+        print("Congrats you won! With a score of ", score)
+    else:
+        print("Game Over! The goalkeeper won with a score of", gkScore)
+
+
 
 main()
-
-
-    
