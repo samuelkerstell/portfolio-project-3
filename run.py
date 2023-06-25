61b0a5165790fa902dae44a1aea8bb2bb9629154
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-06-25 17:33:03 +0000

    Add a player class

--- run.py
+++ run.py
@@ -1,11 +1,33 @@
 import random
 
 
-def goalCheck(computerChoice, userChoice, score, gkScore):
+
+class Player():
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def validate_user_choice(self, value):
+        """
+        Validated the user input so no wrong answers can be submitted
+        """
+        try:
+            if value.lower() not in ["left", "right", "middle"]:
+                raise ValueError(
+                    f"Wrong value submitted you provided {value}"
+                )
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            return False
+
+        return True
+
+
+def goalCheck(computerChoice, userChoice, user, gkScore):
     """
     Checks the user input against the randomized computer choice
-    If they are equal the penalty kick was saved
-    If they are not equal penalty kick was scored
+    If they are equal the penalty kick was saved and the goalkeeper gets one point
+    If they are not equal penalty kick was scored and the user gets one point
     """
     if computerChoice == userChoice:
         gkScore += 1
@@ -13,44 +35,29 @@ def goalCheck(computerChoice, userChoice, score, gkScore):
         print("The goalkeeper has saved your attempt!")
         print("------------------------------------------------------------\n")
     else:
-        score += 1
+        user.score += 1
         print("------------------------------------------------------------")
         print("You scored!")
         print("------------------------------------------------------------\n")
     print("Goalkeeper score is " + str(gkScore))
-    print("Your score is " + str(score))
+    print(f"{user.name}'s score is " + str(user.score))
     print("")
     print("--------------------------------------------------------------")
-    return score, gkScore
-
-
-def validate_user_choice(value):
-    """
-    Validated the user input so no wrong answers can be submitted
-    """
-    try:
-        if value.lower() not in ["left", "right", "middle"]:
-            raise ValueError(
-                f"Wrong value submitted you provided {value}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
-
-    return True
+    return user.score, gkScore
 
 
 def main():
     """
     Run all game functions
     """
-    gkScore = 0
-    score = 0
-    options = ["left", "right", "middle"]
     print("--------------------------------------------------------------")
     print("Welcome to the penalty shootout, first to 5.")
     print("--------------------------------------------------------------")
-    while score < 5 and gkScore < 5:
+    gkScore = 0
+    name = input("Enter your name: ")
+    user = Player(name)
+    options = ["left", "right", "middle"]
+    while user.score < 5 and gkScore < 5:
         while True:
             print("Please choose where you would like to shoot your penalty")
             print("Data should only contain: left, right, or middle")
@@ -58,16 +65,16 @@ def main():
 
             userChoice = input("Enter your answer here: ")
 
-            if validate_user_choice(userChoice):
+            if user.validate_user_choice(userChoice):
                 break
 
         computerChoice = random.choice(options)
-        score, gkScore = goalCheck(computerChoice, userChoice, score, gkScore)
+        user.score, gkScore = goalCheck(computerChoice, userChoice, user, gkScore)
         print()
-    if score == 5:
-        print("Congrats you won! With a score of ", score)
+    if user.score == 5:
+        print(f"Congrats, {user.name}! You won with a score of {user.score}")
     else:
-        print("Game Over! The goalkeeper won with a score of", gkScore)
+        print(f"Game Over! The goalkeeper won with a score of {gkScore}")
 
-
-main()
+if __name__ == '__main__':
+    main()
